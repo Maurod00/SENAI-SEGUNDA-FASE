c27d8928cfd02d7d4b8775d8f8e649097b4623de
Maurod00 <mauriciosbd99@gmail.com>
2026-03-02 19:19:27 -0300

    exercicios de revisao

---
+++ revisao-html-js/exercicios-js/ex16.js
@@ -0,0 +1,43 @@
+const usuarios = [
+  {
+    nome: 'Melba',
+    sobrenome: 'Stein',
+    idade: 15,
+    temAnimalEstimacao: false,
+  },
+  {
+    nome: 'Donovan',
+    sobrenome: 'Barrera',
+    idade: 18,
+    temAnimalEstimacao: false,
+  },
+  {
+    nome: 'Gross',
+    sobrenome: 'Maldonado',
+    idade: 25,
+    temAnimalEstimacao: true,
+  },
+  {
+    nome: 'Massey',
+    sobrenome: 'Waller',
+    idade: 16,
+    temAnimalEstimacao: true,
+  },
+  {
+    nome: 'Harris',
+    sobrenome: 'Bowers',
+    idade: 22,
+    temAnimalEstimacao: true,
+  },
+  {
+    nome: 'Roberson',
+    sobrenome: 'Hancock',
+    idade: 30,
+    temAnimalEstimacao: false,
+  },
+];
+
+
+const maioresDeIdade = usuarios.filter(usuario => usuario.idade >= 18);
+
+console.log(maioresDeIdade);
---
+++ revisao-html-js/exercicios-js/ex19.js
@@ -0,0 +1,15 @@
+const numeros = [18, 7, 15, 19, 53, 12, 40, 30, 34, 66];
+
+const resultado = numeros.map(numero => {
+  if (numero % 3 === 0 && numero % 5 === 0) {
+    return "FizzBuzz";
+  } else if (numero % 3 === 0) {
+    return "Fizz";
+  } else if (numero % 5 === 0) {
+    return "Buzz";
+  } else {
+    return numero;
+  }
+});
+
+console.log(resultado);
